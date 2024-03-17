1cc183197358822df1939a908655643e4ab98b29
KahSiong1221 <jiaxiang1221@gmail.com>
2024-03-17 16:47:24 +0000

    fix

--- include/utils.h
+++ include/utils.h
@@ -20,7 +20,7 @@ void check_upload(struct tm);
 int lock_dir(char *);
 int unlock_dir(char *, mode_t);
 void auto_backup_transfer_reports(struct tm);
-int is_report(const char *, char *[]);
+int is_report(const char *, char *);
 void copy_report(const char *, const char *);
 void remove_report(const char *);
 
